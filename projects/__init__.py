26c5d02c198f456613092f832dab311ebdf10622
Timothy Legge <timlegge@gmail.com>
2015-02-20 02:21:25 +0000

    add simple menu

---
+++ projects/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Accounts are created to seperate groups of users
+# think for a company or system that might have several users or directorieies
+def create(name):
+	# create a new account
